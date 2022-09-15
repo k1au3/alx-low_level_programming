97f95515ed2ae842206302f1baf30e1d84daa23d
k1au3 <lindquistconcern@gmail>
2022-09-15 00:38:47 -0700

    check uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
 *_isupper - function that verifies if a  character is uppercase or not
 *@c: tested character
-*Return: returns 1 if it is uppercase , 0 if not
+*Return: returns 1 if it's uppercase , 0 if not
 */
 
 int _isupper(int c)
